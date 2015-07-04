38aba18f6c47ad8b07933b48c833f7089e341393
BastienLaby <bastien.laby@gmail.com>
2015-07-04 18:22:18 +0200

    Le joueur Rift peut avancer/reculer et se déplacer droite/gauche

---
+++ VirtualDevCamp1/Assets/RiftPlayer_Walk.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RiftPlayer_Walk : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate() {
+		float dx = Input.GetAxis ("Horizontal");
+		float dz = Input.GetAxis ("Vertical");
+		Vector3 currentPos = GetComponent<Rigidbody> ().position;
+		GetComponent<Rigidbody> ().position = new Vector3 (
+			currentPos.x + dz * speed,
+			currentPos.y,
+			currentPos.z + -dx * speed);
+	}
+}
