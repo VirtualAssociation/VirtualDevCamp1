abcc70a7dc4e6a2056d25a6121f9a57648ca276e
Victor Viale <somebodywas@gmail.com>
2015-07-04 18:48:29 +0200

    ajout GUI main menu et builder

---
+++ VirtualDevCamp1/Assets/Scripts/MainMenuGUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuGUI : MonoBehaviour
+{
+    public int width, height;
+    void OnGUI()
+    {
+        
+        GUI.Box(new Rect(Screen.width/2-width/2, Screen.height/2-height/2, width, height), "Menu Principal");
+
+        if (GUI.Button(new Rect(Screen.width / 2 - width / 2 + 10, 30 + Screen.height / 2 - height / 2, width-20, 20), "Jouer"))
+        {
+            //todo
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 - width / 2 + 10, 30 + Screen.height / 2 - height / 2 + 50, width - 20, 20), "Options"))
+        {
+            //todo
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 - width / 2 + 10, 30 + Screen.height / 2 - height / 2 + 100, width - 20, 20), "Quitter"))
+        {
+            Application.Quit();
+        }
+    }
+}
